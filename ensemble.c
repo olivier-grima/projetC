675a5034887628e71a663b210ce4d82d5da8e242
oceanemofid <oceane.mofid@gmail.com>
2020-12-26 22:55:39 +0100

    update

--- ensemble.c
+++ ensemble.c
@@ -5,7 +5,7 @@
 //ensemble e = {1,4,5} de capacité 10
 //présence de l'entier -> 1  
 //absence de l'entier -> 0  
-//tableau e = [3,10,0,1,0,0,1,1,0,0,0,0]
+//tableau e = {3,10,0,1,0,0,1,1,0,0,0,0}
 
 //-----------------------------Déclarations------------------------------------------//
 
@@ -14,57 +14,44 @@
 #include <math.h>
 #define CAPACITEMAX 1000
 typedef int ensemble[CAPACITEMAX+2];
+#define getName(var)  #var
+
 
 //-----------------------------Procédures------------------------------------------//
 
-void initEns(ensemble e,int n){ 
-    ensemble ens[n+2]; //initialise un tableau de capacité n
+void initEns(ensemble e,int n){ //initialise un tableau de capacité n
+    ensemble e; 
 }
 
 //
 
 void plein(ensemble e){ // remplit l'ensemble passé en paramètre
-    int N=e[1];
-    //e[0]=?; //cardinalité effective -> nbr d'éléments
-    //e[1] = {N-2}; //capacité effective -> capacité
-    for(int i = 2; i<=N-1; i++){
-        //e[i]=1; -> error assignment
+    e[0]=e[1];
+    for(int i = 2; i<=e[1]+1; i++){
+        e[i]=1;
     }
-    //not finished
 }
 
 //
 
-int egal(ensemble e1,ensemble e2){
-    int egaux;
+int egal(ensemble e1,ensemble e2){ //hypothèse : les ensembles font la même taille -> capacités égales
     int i=2;
-    if (e1[0]==e2[0]){ //test si même nbr d'élément
-        //tester chaque élém ? condition : avoir même capacité pour pouvoir comparer ?
-        //int max = fmax(e1[1], e2[1]); //prend la capacité max 
-
-        for(int k=2;k<CAPACITEMAX+2;k++){
-            //compare chaque élément (0 ou 1)
-           if(e1[i]==e2[i]){
-                egaux=1;
-                i++;
-            } 
-            else {
-                egaux=0; //s'arrête dès que des elem sont différents 
+    if (e1[0]==e2[0]){ //test si même nbr d'élément 
+        for(int k=2; k<e1[1]+1; k++){
+           if(e1[k]!=e2[k]){ //s'arrête dès que des elem ont des val différentes 
                 return 0; //ensembles différents
-            }
-        //boucle infinie ??
+            } 
         }
         return 1; //ensembles égaux
     }
     else{
-        return 0;
-    }     //ensembles différents
-    //not finished
+        return 0; //ensembles ne comportent pas le même nbr d'éléments -> forcément non égaux
+    }    
 }
 
 //
 
-int appartient(int n,ensemble e){ //teste si entier n appartient à un ensemble e ;
+int appartient(int n, ensemble e){ //teste si entier n appartient à un ensemble e ;
     if(e[n+2]==n){
         return 1;
     }
@@ -73,27 +60,32 @@ int appartient(int n,ensemble e){ //teste si entier n appartient à un ensemble 
 
 //
 
-int enlever(int n,ensemble e){ //enlève un entier n d’un ensemble e ;
-    
+int enlever(int n, ensemble e){ //enlève un entier n d’un ensemble e ;
     e[n+2]=0;
     return EXIT_SUCCESS;
 }
 
-void printlnEns(ensemble e){ //printlnEns (1 paramètre), écrit un ensemble e sur la sortie standard 
-                        //suivi d’un passage à la ligne ; e1 = [ 2 19 31 33 ]
-    printf("%s = "); //nom de l'array
-    for (int i = 2; i < e[1]-1; i++){
-        printf("%d",e[i]); 
+//
+
+void printlnEns(ensemble e){ //écrit un ensemble e sur la sortie standard suivi d’un passage à la ligne ; e1 = [ 2 19 31 33 ]
+    printf("ens = [ "); //nom de l'array
+    for (int i = 2; i <=e[1]+1; i++){
+        if(e[i]==1){
+            printf("%d ",i-2);
+        } 
     }
     printf("]\n");
-
 }
 
 //
 
-void Union(int x,int y,int z){
-
-
+void Union(ensemble e1, ensemble e2, ensemble e3){ //calcule e3 = e1 U e2 ;
+    for (int i = 2; i <=e1[1]+1; i++){
+        if(e1[i]==1 || e2[i]==1){
+            e3[i]=1;
+        }
+    }
+    //not finished
 }
 
 //
@@ -108,7 +100,6 @@ void complementaire(int x,int y){
 //-----------------------------Test------------------------------------------//
 
 int main(){
-
     printf("hello world");
     return EXIT_SUCCESS;
 }
--- test.c
+++ test.c
@@ -2,9 +2,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#define CAPACITEMAX 1000
+typedef int ensemble[CAPACITEMAX+2];
+
+void printlnEns(ensemble e){ //écrit un ensemble e sur la sortie standard suivi d’un passage à la ligne ; e1 = [ 2 19 31 33 ]
+    printf("ens = [ "); //nom de l'array
+    for (int i = 2; i <=e[1]+1; i++){
+        if(e[i]==1){
+            printf("%d ",i-2);
+        } 
+    }
+    printf("]\n");
+}
 
 int main(){
-    
-    printf("hello world");  
+    ensemble ens = {3,10,1,1,1,1,1,0,1,0,0,1}; //ens = [0 1 2 4]
+    printlnEns(ens); 
     return EXIT_SUCCESS;
 }
