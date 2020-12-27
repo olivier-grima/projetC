9c199fb6d8d18cf544e3d55ca5bd6fe95208ff29
Olivier GRIMA <olivier.grima@etu.univ-cotedazur.fr>
2020-12-27 21:19:47 +0100

    test inutile

--- test2.c
+++ test2.c
@@ -9,8 +9,8 @@ typedef int ensemble[CAPACITEMAX+2];
 
 void printlnEns(ensemble e){ //écrit un ensemble e sur la sortie standard suivi d’un passage à la ligne ; e1 = [ 2 19 31 33 ]
     //printf("%s = [ ", getName(e)); //nom de l'array
-    char nom[]= e;
-    printf("%s =[ ", e);
+    //char nom[]= e;
+    //printf("%s =[ ", e);
     for (int i = 2; i <=e[1]+1; i++){
         if(e[i]==1){
             printf("%d ",i-2);
@@ -31,7 +31,9 @@ void affecter(ensemble e1, ensemble e2){// affecte un ensemble e2 à un ensemble
 int main(){
     ensemble ens = {4,5,1,1,1,0,1}; //ens = [ 0 1 2 4 ]
     ensemble ens1 = {3,5,0,1,1,1,0}; //ens1 = [ 1 2 ]
+    printf("ens = [");
     printlnEns(ens);
+    printf("ens1 = [");
     printlnEns(ens1);
     affecter(ens, ens1);
     return EXIT_SUCCESS;
